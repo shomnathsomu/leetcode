87c43dc3bda6598fb99786ca29131a997d7fbd61
Shomnath Pramanik <shomnathcse22@gmail.com>
2022-04-12 00:26:24 +0600

    negative marking approach

---
+++ Array/287-find-duplicate-number.cpp
@@ -0,0 +1,39 @@
+vector<int> nums;
+
+class Solution{
+public:
+    int findDuplicate(vector<int>& nums){
+        int duplicate=-1;
+        for(int i=0; i<nums.size(); i++){
+            int cur = abs(nums[i]);
+            if(nums[cur] < 0){
+                duplicate = cur;
+                break ;
+            }
+
+            nums[cur] = nums[cur]*-1;
+        }
+
+        for(auto& num: nums)
+            num = abs(num);
+
+        return duplicate;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+
+    DI(n);
+    int num;
+    for(int i=0; i<n; i++) {
+        scanf("%d", &num);
+        nums.push_back(num);
+    }
+
+    Solution sol;
+    int res = sol.findDuplicate(nums);
+
+    printf("%d\n", res);
+
+    return 0;
+}
